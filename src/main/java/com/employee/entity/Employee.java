b47e8e8551a48d6681a8dc8a16c3ca0f048e735e
anandkumarjha318@gmail.com <anandkumarjha318@gmail.com>
2024-01-11 21:57:55 +0530

    correction in name of employee class

--- src/main/java/com/employee/entity/Employee.java
+++ src/main/java/com/employee/entity/Employee.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Empolyee {
+public class Employee {
 	private Integer empId;
 	private String name;
 	private String address;
--- src/main/java/com/employee/repository/EmployeeRepository.java
+++ src/main/java/com/employee/repository/EmployeeRepository.java
@@ -3,9 +3,9 @@ package com.employee.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.employee.entity.Empolyee;
+import com.employee.entity.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Empolyee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
