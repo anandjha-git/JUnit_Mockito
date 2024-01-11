84695a7b63f1723d5368d818e4a357c80bf574fe
anandkumarjha318@gmail.com <anandkumarjha318@gmail.com>
2024-01-11 22:23:17 +0530

    created custom method in repository

--- src/main/java/com/employee/repository/EmployeeRepository.java
+++ src/main/java/com/employee/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.employee.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.employee.entity.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	Employee getByName(String name);
+
+	List<Employee> getAllByName(String name);
 }
