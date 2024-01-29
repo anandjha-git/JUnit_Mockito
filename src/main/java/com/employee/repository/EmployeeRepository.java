dd8974e0fed3f98606313ffd801c46a1714826be
anandkumarjha318@gmail.com <anandkumarjha318@gmail.com>
2024-01-29 10:13:19 +0530

    updated repository

--- src/main/java/com/employee/repository/EmployeeRepository.java
+++ src/main/java/com/employee/repository/EmployeeRepository.java
@@ -3,14 +3,15 @@ package com.employee.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.employee.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+	@Query(value="select e from employee where e.name=?1")
 	Employee getByName(String name);
-
+	@Query(value="select * from employee where name=?1")
 	List<Employee> getAllByName(String name);
 }
